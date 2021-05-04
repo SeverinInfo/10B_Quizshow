1739bc2822d9eeecbad574870ecf5051804aa78b
noah.henze@t-online.de <noah.henze@t-online.de>
2021-05-04 09:59:24 +0200

    Frage 1 wird frage 10 + antworten für frage 10

--- FRAGE1.java
+++ FRAGE1.java
@@ -4,9 +4,9 @@ public class FRAGE1
 {
     String Frage = "Wofür steht die KGaA";
     String Antf1 = "Kaum Götter aßen Ameisen";
-    String Antf2 = "wgwg";
-    String Antf3 = "wgww";
-    String Antt = "wgwgg";
+    String Antf2 = "Kommanditgesellschaft auf Aktien";
+    String Antf3 = "Koordinationsgesellschaft ab Aristoteles";
+    String Antf4 = "Kommandogesetz aus Australien";
     
     public FRAGE1()
     {
