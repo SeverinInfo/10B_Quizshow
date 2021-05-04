5367e01b60abaf835af42c116d277f8a670d7ba0
Severin <Severin.Hanf@gmail.com>
2021-05-04 09:24:48 +0200

    FragenBeispiel

--- FRAGE1.java
+++ FRAGE1.java
@@ -1,33 +1,17 @@
 
-/**
- * Beschreiben Sie hier die Klasse FRAGE1.
- * 
- * @author (Ihr Name) 
- * @version (eine Versionsnummer oder ein Datum)
- */
+
 public class FRAGE1
 {
-    // Instanzvariablen - ersetzen Sie das folgende Beispiel mit Ihren Variablen
-    private int x;
-
-    /**
-     * Konstruktor für Objekte der Klasse FRAGE1
-     */
+    String Frage = "Wofür steht die KGaA";
+    String Antf1 = "Kaum Götter aßen Ameisen";
+    String Antf2 = "wgwg";
+    String Antf3 = "wgww";
+    String Antt = "wgwgg";
+    
     public FRAGE1()
     {
-        // Instanzvariable initialisieren
-        x = 0;
+        
     }
 
-    /**
-     * Ein Beispiel einer Methode - ersetzen Sie diesen Kommentar mit Ihrem eigenen
-     * 
-     * @param  y    ein Beispielparameter für eine Methode
-     * @return        die Summe aus x und y
-     */
-    public int beispielMethode(int y)
-    {
-        // tragen Sie hier den Code ein
-        return x + y;
-    }
+    
 }
