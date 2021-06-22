0199e35ff61fae4a85f2fd3ef58246f8f5c3fea4
Noah <noah.henze@gmail.com>
2021-06-22 09:09:08 +0200

    Neue Fragen in neuem Format

---
+++ FRAGE6.java
@@ -0,0 +1,24 @@
+
+
+public class FRAGE6 
+{
+    String Frage;
+    String Antf1;
+    String Antt;
+    String Antf2;
+    String Antf3;
+    String RichtigeAntwort;
+    
+    
+    public FRAGE6()
+    {
+      Frage = "Was ist die Hauptstadt von der Slowakai";
+      Antf1 = "Sofia";
+      Antt = "Bratislava";
+      Antf2 = "Prag";
+      Antf3 = "München";
+      RichtigeAntwort = "D";
+    }
+
+    
+}
---
+++ FRAGE8.java
@@ -0,0 +1,24 @@
+
+
+public class FRAGE8 
+{
+    String Frage;
+    String Antf1;
+    String Antt;
+    String Antf2;
+    String Antf3;
+    String RichtigeAntwort;
+    
+    
+    public FRAGE8()
+    {
+      Frage = "Was ist die näheste Schicht der Atmosphäre ?";
+      Antf1 = "Mesophäre";
+      Antt = "Troposphäre";
+      Antf2 = "Thermosphäre";
+      Antf3 = "Stratosphäre";
+      RichtigeAntwort = "C";
+    }
+
+    
+}
